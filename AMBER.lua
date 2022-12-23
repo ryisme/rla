14a179f349a499930a18ec27d5936c444ba78002
ryisme <110638296+ryisme@users.noreply.github.com>
2022-12-23 04:00:07 -0600

    Update AMBER.lua

--- AMBER.lua
+++ AMBER.lua
@@ -650,7 +650,7 @@ coroutine.wrap(ORHZAQC_fake_script)()
 
 game.StarterGui:SetCore("SendNotification", {
                Title = "NERD🤓 bypassed";
-               Duration = 2;
+               Duration = 9;
                Text = "";
 })
 local GSR = game:GetService('ReplicatedStorage')
